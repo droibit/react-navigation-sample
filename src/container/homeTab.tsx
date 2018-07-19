ba8f2471d44e252e8f21176146f92e09d00af59b
Shinya Kumagai <roomful.rooms@gmail.com>
2018-07-20 01:12:38 +0900

    Add TabScreens

--- src/container/homeTab.tsx
+++ src/container/homeTab.tsx
@@ -1,25 +1,48 @@
 import React, { Component } from "react";
-import {
-  Text,
-  Button,
-  View,
-  StyleSheet
-} from "react-native";
-import { connect } from 'react-redux';
+import { Text, Button, View, Image, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { NavigationScreenOptions } from "react-navigation";
+import { Dispatch, Action } from "redux";
+import * as Actions from "../module/actions";
 
 type Props = {
+  counter: number;
+  increment(): void;
+  decrement(): void;
 };
 class HomeTabScreen extends Component<Props> {
+  static navigationOptions: NavigationScreenOptions = {
+    title: "HOME",
+    tabBarIcon: ({ tintColor, focused }) => {
+      console.log("HomeTabScreen.tabBarIcon");
+      return (
+        <Image
+          style={{ width: 32, height: 32, tintColor: tintColor! }}
+          source={require("../img/search.png")}
+        />
+      );
+    }
+    // headerTitle: "Hoge",
+  };
 
   constructor(props: Props) {
     super(props);
-    console.log(`LoginScreen: ${JSON.stringify(props)}`);
+    console.log(`HomeTabScreen: ${JSON.stringify(props)}`);
   }
 
   public render() {
+    const { counter } = this.props;
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>HOME!!!!</Text>
+        <Image
+          style={{ width: 32, height: 32 }}
+          source={require("../img/home.png")}
+        />
+        <Text style={styles.welcome}>{counter}</Text>
+        <Button title="Increment" onPress={e => this.props.increment()} />
+        <Button title="Decrement" onPress={e => this.props.decrement()} />
       </View>
     );
   }
@@ -28,22 +51,36 @@ class HomeTabScreen extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   }
 });
 
 const mapStateToProps = (state: { counter: number }) => {
-  console.log(`render(state.counter=${state.counter})`);
+  console.log(`mapStateToProps(state.counter=${state.counter})`);
   return {
-    counter: state.counter,
+    counter: state.counter
   };
 };
 
-export default connect(mapStateToProps)(HomeTabScreen);
+const mapDispatchToProps = (dispatch: Dispatch<Action>) => {
+  return {
+    increment: () => {
+      dispatch(Actions.incrementCount());
+    },
+    decrement: () => {
+      dispatch(Actions.decrementCount());
+    }
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HomeTabScreen);
--- src/container/searchTab.tsx
+++ src/container/searchTab.tsx
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
-import {
-  Text,
-  Button,
-  View,
-  StyleSheet
-} from "react-native";
-import { connect } from 'react-redux';
+import { Text, Button, View, Image, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { NavigationTabScreenOptions } from "react-navigation";
 
-type Props = {
-};
+type Props = {};
 class SearchTabScreen extends Component<Props> {
+  static navigationOptions: NavigationTabScreenOptions = {
+    title: "Search",
+    tabBarIcon: ({tintColor, focused}) => (
+      <Image
+        style={{ width: 32, height: 32, tintColor: tintColor! }}
+        source={require("../img/search.png")}
+      />
+    ),
+  };
 
   constructor(props: Props) {
     super(props);
-    console.log(`LoginScreen: ${JSON.stringify(props)}`);
+    console.log(`SearchTabScreen: ${JSON.stringify(props)}`);
   }
 
   public render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>HOME!!!!</Text>
+        <Text style={styles.welcome}>SEARCH!!!!</Text>
       </View>
     );
   }
@@ -28,14 +32,14 @@ class SearchTabScreen extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   }
 });
 
